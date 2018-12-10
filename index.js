fd00945d3dfb326bffa10a78d1efa7249a96eead
wolfx <digimonx@qq.com>
2018-12-10 11:08:04 +0800

    format

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
    * @param numberEveryGroup
    * @returns {Array}
    */
-  arrayToGroup: function (array, numberEveryGroup) {
+  arrayToGroup: function(array, numberEveryGroup) {
     let result = [];
     for (let i = 0, len = array.length; i < len; i += numberEveryGroup) {
       result.push(array.slice(i, i + numberEveryGroup));
@@ -17,7 +17,7 @@ module.exports = {
    * @param v
    * @returns {string}
    */
-  thousandNumFormat: function (v) {
+  thousandNumFormat: function(v) {
     if (v) {
       v = v.toFixed(2);
       v = parseFloat(v);
@@ -34,12 +34,12 @@ module.exports = {
    * queryString转Object
    * @param str
    */
-  queryStringToObject: function (str) {
-    str = str.replace('?', '');
-    let strArray = str.split('&');
+  queryStringToObject: function(str) {
+    str = str.replace("?", "");
+    let strArray = str.split("&");
     let obj = {};
     for (let i = 0; i < strArray.length; i++) {
-      let params = strArray[i].split('=');
+      let params = strArray[i].split("=");
       obj[params[0]] = params[1];
     }
     return obj;
